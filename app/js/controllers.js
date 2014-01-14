6f699a31ad33c63c7cf078b868facf339c80c987
Gvn Lazar Suntop <gavin@gsuntop.com>
2014-01-14 14:57:39 -0800

    stubbing out JSON to send to service

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,13 +3,38 @@
 angular.module('myApp.controllers', []).
   controller('addEventController', ['$scope', function($scope) {
     $scope.attendees = 5;
+    $scope.attemptedToSubmit = false;
 
     $scope.addEvent = function () {
-      console.log($scope);
+      $scope.attemptedToSubmit = true;
+
+      var event = {
+        title: $scope.title,
+        description: $scope.description,
+        attendees: $scope.attendees,
+        beginDate: $scope.beginDate,
+        beginTime: $scope.beginTime,
+        endDate: $scope.endDate,
+        endTime: $scope.endTime,
+        registerLink: $scope.registerLink
+        // latitude: ,
+        // longitude: ,
+        // city: ,
+        // country: ,
+        // picture: ,
+        // organizer: ,
+        // organizerId ,
+        // featured
+      }
+
+      console.log(event);
     }
   }])
   .controller('eventDetailController', [function() {
 
+  }])
+  .controller('navController', ['$scope', '$location', function($scope, $location) {
+    $scope.isActive = function (location) {
+      return location === $location.path();
+    }
   }]);
-
-
