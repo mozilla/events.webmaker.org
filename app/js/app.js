5408f24019101e8e0693a1cf21c7ee8a55533a2e
Gvn Lazar Suntop <gavin@gsuntop.com>
2014-01-16 15:14:38 -0800

    reset scrollbar on view change

--- app/js/app.js
+++ app/js/app.js
@@ -6,10 +6,33 @@ angular.module('myApp', [
   'myApp.directives',
   'myApp.controllers'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/add', {templateUrl: 'views/add.html', controller: 'addEventController'});
-  $routeProvider.when('/events', {templateUrl: 'views/list.html', controller: 'eventListController'});
-  $routeProvider.when('/events/:id', {templateUrl: 'views/detail.html', controller: 'eventDetailController'});
-  $routeProvider.when('/edit/:id', {templateUrl: 'views/edit.html', controller: 'eventEditController'});
-  $routeProvider.otherwise({redirectTo: '/events'});
-}]);
+config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.when('/add', {
+      templateUrl: 'views/add.html',
+      controller: 'addEventController'
+    });
+    $routeProvider.when('/events', {
+      templateUrl: 'views/list.html',
+      controller: 'eventListController'
+    });
+    $routeProvider.when('/events/:id', {
+      templateUrl: 'views/detail.html',
+      controller: 'eventDetailController'
+    });
+    $routeProvider.when('/edit/:id', {
+      templateUrl: 'views/edit.html',
+      controller: 'eventEditController'
+    });
+    $routeProvider.otherwise({
+      redirectTo: '/events'
+    });
+  }
+]).
+run(['$rootScope',
+  function ($rootScope) {
+    $rootScope.$on('$locationChangeSuccess', function (event) {
+      window.scrollTo(0, 0);
+    });
+  }
+]);
