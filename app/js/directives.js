8cd774c09c86a365ea132454337dba8ee09c7561
Gvn Lazar Suntop <gavin@gsuntop.com>
2014-02-12 12:55:36 -0800

    adding location autocompletion w. google's service

--- app/js/directives.js
+++ app/js/directives.js
@@ -8,6 +8,43 @@ angular.module('myApp.directives', [])
       };
     }
   ])
+  .directive('autocompleteLocation', function () {
+    return {
+      restrict: 'A',
+      controller: ['$element', '$scope',
+        function ($element, $scope) {
+          var autocomplete = new google.maps.places.Autocomplete($element[0], {});
+
+          autocomplete.addListener('place_changed', function () {
+            var placeData = autocomplete.getPlace();
+
+            var city;
+            var country;
+
+            placeData.address_components.forEach(function (component) {
+              if (component.types[0] === 'locality') {
+                city = component.long_name;
+              } else if (component.types[0] === 'country') {
+                country = component.long_name;
+              }
+            });
+
+            // Directly attach address info to scope.
+            // TODO: This is kind of gross because it's a 2-way dep.
+            //       Look into firing event or some other non domain-specific method
+            $scope.event.latitude = placeData.geometry.location.lat();
+            $scope.event.longitude = placeData.geometry.location.lng();
+            $scope.event.city = city;
+            $scope.event.country = country;
+
+            // Force view model to update after autocompletion
+            // TODO: This is kind of gross too. What's the "angular way"?
+            $element.trigger('input');
+          });
+        }
+      ]
+    };
+  })
   .directive('eventForm', function () {
     return {
       restrict: 'E',
